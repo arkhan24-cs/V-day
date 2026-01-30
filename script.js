1d37b47b9f7d0ee0f65da940ff6136a545d439af
Rafay K <114795606+arkhan24-cs@users.noreply.github.com>
2026-01-30 03:00:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function displayCat() {
   // Create a new Image element for the cat
   var catImage = new Image();
   // Set the source (file path) for the cat image
-  catImage.src = "cat.gif"; // Assuming the cat image is named "cat.gif"
+  catImage.src = "IMG_9731.jpg"; // Assuming the cat image is named "cat.gif"
   // Set alternative text for the image (for accessibility)
   catImage.alt = "Cat";
   // When the cat image is fully loaded, add it to the image container
